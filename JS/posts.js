cffa2f7543bd4332d704b7d2554a96af9b1a4a63
mdfahim <mdfahimislamoritro@gmail.com>
2023-05-07 08:58:13 +0600

    New-Api-practice-Added

--- JS/posts.js
+++ JS/posts.js
@@ -1,1 +1,23 @@
-console.log('posts jora lagce')
+function loadpost(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+.then(res => res.json())
+.then(data =>displayPosts(data))
+};
+
+
+function displayPosts(posts){
+    const postContainer = document.getElementById('post-container')
+    for(const post of posts){
+        const postDiv = document.createElement('div');
+        postDiv.classList.add('post')
+        console.log(post)
+        postDiv.innerHTML=`
+        <h4>user-${post.userId}</h4>
+        <h5>Post:${post.title}</h5>
+        <p>Post Description${post.body}</p>
+        `
+        postContainer.appendChild(postDiv);
+    }
+}
+
+loadpost()
